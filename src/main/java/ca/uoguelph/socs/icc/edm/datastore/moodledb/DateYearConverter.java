ad77b700676d2cead48953ed3fefce8749c21beb
James E. Stark <jstark@ieee.org>
2014-11-27 22:49:44 -0500

    Add GPL Boiler plate and fix package names.

--- src/main/java/ca/uoguelph/socs/icc/edm/datastore/moodledb/DateYearConverter.java
+++ src/main/java/ca/uoguelph/socs/icc/edm/datastore/moodledb/DateYearConverter.java
@@ -14,7 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package ca.uoguelph.socs.icc.edm.moodledb;
+package ca.uoguelph.socs.icc.edm.datastore.moodledb;
 
 import java.util.Calendar;
 
--- src/main/java/ca/uoguelph/socs/icc/edm/datastore/moodledb/MoodleEnrolmentUserData.java
+++ src/main/java/ca/uoguelph/socs/icc/edm/datastore/moodledb/MoodleEnrolmentUserData.java
@@ -1,12 +1,28 @@
+/* Copyright (C) 2014 James E. Stark
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package ca.uoguelph.socs.icc.edm.moodledb;
 
 import java.util.Set;
 import java.util.HashSet;
 
 import ca.uoguelph.socs.icc.edm.domain.Role;
-import ca.uoguelph.socs.icc.edm.domain.EnrolledUser;
+import ca.uoguelph.socs.icc.edm.domain.UserEnrolmentData;
 
-class MoodleEnrolmentUserData extends EnrolledUser
+class MoodleEnrolmentUserData extends UserEnrolmentData
 {
 	private Set<Role> roles;
 
