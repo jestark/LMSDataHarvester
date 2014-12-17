2aace63495971a429de0700279ee646c957aa766
James E. Stark <jstark@ieee.org>
2014-12-17 01:37:48 -0500

    Import top-level builder and manager factory classes.

---
+++ src/main/java/ca/uoguelph/socs/icc/edm/domain/AbstractBuilder.java
@@ -0,0 +1,51 @@
+/* Copyright (C) 2014 James E. Stark
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ca.uoguelph.socs.icc.edm.domain;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import ca.uoguelph.socs.icc.edm.datastore.IdGenerator;
+
+public abstract class AbstractBuilder<T extends DomainModelElement>
+{
+	/** The manager (used to add new instances to the model) */
+	private final DomainModelManager<T> manager;
+
+	/** The ID number generator */
+//	protected final IdGenerator generator;
+
+	/** The Logger */
+	private final Log log;
+
+	protected AbstractBuilder (DomainModelManager<T> manager)
+	{
+		this.log = LogFactory.getLog (AbstractBuilder.class);
+		
+		this.manager = manager;
+//		this.generator = generator;
+	}
+
+	public final T create ()
+	{
+		return this.build ();
+	}
+
+	protected abstract T build ();
+
+	public abstract void clear ();
+}
---
+++ src/main/java/ca/uoguelph/socs/icc/edm/domain/AbstractManagerFactory.java
@@ -0,0 +1,87 @@
+/* Copyright (C) 2014 James E. Stark
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ca.uoguelph.socs.icc.edm.domain;
+
+import java.util.Map;
+import java.util.Set;
+
+import java.util.HashMap;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import ca.uoguelph.socs.icc.edm.datastore.DataStoreQuery;
+import ca.uoguelph.socs.icc.edm.datastore.IdGenerator;
+
+public abstract class AbstractManagerFactory<T extends DomainModelElement, X extends DomainModelManager<T>>
+{
+	private X manager;
+
+	private Log log;
+
+	protected AbstractManagerFactory ()
+	{
+		this.manager = null;
+
+		this.log = LogFactory.getLog (AbstractManagerFactory.class);
+	}
+
+	public final void registerBuilder (Class<?> impl, BuilderFactory<T> factory)
+	{
+	}
+
+	public final void registerQuery (Class<?> impl, QueryFactory<T> factory)
+	{
+	}
+
+	public final Set<Class<?>> getRegisteredBuilders ()
+	{
+		return null;
+	}
+
+	public final Set<Class<?>> getRegisteredQueries ()
+	{
+		return null;
+	}
+
+	protected final DataStoreQuery<T> createQuery (DomainModel model)
+	{
+		return null;
+	}
+
+	protected final DomainModelBuilder<T> createBuilder (DomainModel model)
+	{
+		return null;
+	}
+
+	protected abstract X needsaname (DomainModel model);
+
+	protected final X createManager (DomainModel model)
+	{
+		if (this.manager == null)
+		{
+			this.manager = needsaname (model);
+		}
+
+		return this.manager;
+	}
+
+	public final X getManager (DomainModel model)
+	{
+		return this.createManager (model);
+	}
+}
