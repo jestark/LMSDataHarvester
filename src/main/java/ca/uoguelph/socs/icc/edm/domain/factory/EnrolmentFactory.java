54372f26213758c485fc10d89c8f38e84faa45ac
James E. Stark <jstark@ieee.org>
2014-12-20 10:52:53 -0500

    Add factory classes for the remaining domain model interfaces.

---
+++ src/main/java/ca/uoguelph/socs/icc/edm/domain/factory/EnrolmentFactory.java
@@ -0,0 +1,46 @@
+/* Copyright (C) 2014 James E. Stark
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ca.uoguelph.socs.icc.edm.domain.factory;
+
+import ca.uoguelph.socs.icc.edm.domain.AbstractManagerFactory;
+import ca.uoguelph.socs.icc.edm.domain.Enrolment;
+import ca.uoguelph.socs.icc.edm.domain.EnrolmentManager;
+
+public final class EnrolmentFactory extends AbstractManagerFactory<Enrolment, EnrolmentManager>
+{
+	private static EnrolmentFactory instance;
+
+	static
+	{
+		EnrolmentFactory.instance = null;
+	}
+
+	public static EnrolmentFactory getInstance ()
+	{
+		if (EnrolmentFactory.instance == null)
+		{
+			EnrolmentFactory.instance = new EnrolmentFactory ();
+		}
+
+		return EnrolmentFactory.instance;
+	}
+
+	private EnrolmentFactory ()
+	{
+		super ();
+	}
+}
---
+++ src/main/java/ca/uoguelph/socs/icc/edm/domain/factory/RoleFactory.java
@@ -0,0 +1,46 @@
+/* Copyright (C) 2014 James E. Stark
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ca.uoguelph.socs.icc.edm.domain.factory;
+
+import ca.uoguelph.socs.icc.edm.domain.AbstractManagerFactory;
+import ca.uoguelph.socs.icc.edm.domain.Role;
+import ca.uoguelph.socs.icc.edm.domain.RoleManager;
+
+public final class RoleFactory extends AbstractManagerFactory<Role, RoleManager>
+{
+	private static RoleFactory instance;
+
+	static
+	{
+		RoleFactory.instance = null;
+	}
+
+	public static RoleFactory getInstance ()
+	{
+		if (RoleFactory.instance == null)
+		{
+			RoleFactory.instance = new RoleFactory ();
+		}
+
+		return RoleFactory.instance;
+	}
+
+	private RoleFactory ()
+	{
+		super ();
+	}
+}
