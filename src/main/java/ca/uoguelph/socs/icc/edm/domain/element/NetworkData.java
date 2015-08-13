fb1b868ab87a0826b9f2b18fe6b304c3ff09ea0d
James E. Stark <jstark@ieee.org>
2015-08-13 14:02:26 -0400

    Add a Network implementation

---
+++ src/main/java/ca/uoguelph/socs/icc/edm/domain/element/NetworkData.java
@@ -0,0 +1,196 @@
+/* Copyright (C) 2014, 2015 James E. Stark
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ca.uoguelph.socs.icc.edm.domain.element;
+
+import java.io.Serializable;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import ca.uoguelph.socs.icc.edm.domain.Network;
+
+import ca.uoguelph.socs.icc.edm.domain.datastore.Profile;
+
+import ca.uoguelph.socs.icc.edm.domain.metadata.Implementation;
+
+/**
+ * Implementation of the <code>Network</code> interface.  It is expected that
+ * instances of this class will be accessed though the <code>Network</code>
+ * interface, along with the relevant manager, and builder.  See the
+ * <code>Network</code> interface documentation for details.
+ *
+ * @author  James E. Stark
+ * @version 1.0
+ */
+
+
+public class NetworkData extends Network implements Serializable
+{
+	/** Serial version id, required by the Serializable interface */
+	private static final long serialVersionUID = 1L;
+
+	/** The primary key for the <code>Network</code> */
+	private Long id;
+
+	/** The name of the <code>Network</code> */
+	private String name;
+
+	/**
+	 * Static initializer to register the <code>NetworkData</code> class with
+	 * the factories.
+	 */
+
+	static
+	{
+		Profile.registerCreator (Implementation.getInstance (Network.metadata, NetworkData.class, NetworkData::new));
+	}
+
+	/**
+	 * Create the <code>Network</code> with null values.
+	 */
+
+	protected NetworkData ()
+	{
+		this.id = null;
+		this.name = null;
+	}
+
+	/**
+	 * Compare two <code>Network</code> instances to determine if they are
+	 * equal.  The <code>Network</code> instances are compared based upon their
+	 * names.
+	 *
+	 * @param  obj The <code>Network</code> instance to compare to the one
+	 *             represented by the called instance
+	 *
+	 * @return     <code>True</code> if the two <code>Network</code> instances
+	 *             are equal, <code>False</code> otherwise
+	 */
+
+	@Override
+	public boolean equals (final Object obj)
+	{
+		boolean result = false;
+
+		if (obj == this)
+		{
+			result = true;
+		}
+		else if (obj instanceof Network)
+		{
+			EqualsBuilder ebuilder = new EqualsBuilder ();
+			ebuilder.append (this.name, ((Network) obj).getName ());
+
+			result = ebuilder.isEquals ();
+		}
+
+		return result;
+	}
+
+	/**
+	 * Compute a <code>hashCode</code> of the <code>Network</code> instance.
+	 * The hash code is computed based upon the name of the instance.
+	 *
+	 * @return An <code>Integer</code> containing the hash code
+	 */
+
+	@Override
+	public int hashCode ()
+	{
+		final int base = 1069;
+		final int mult = 919;
+
+		HashCodeBuilder hbuilder = new HashCodeBuilder (base, mult);
+		hbuilder.append (this.name);
+
+		return hbuilder.toHashCode ();
+	}
+
+	/**
+	 * Get the <code>DataStore</code> identifier for the <code>Network</code>
+	 * instance.
+	 *
+	 * @return a Long integer containing <code>DataStore</code> identifier
+	 */
+
+	public Long getId ()
+	{
+		return this.id;
+	}
+
+	/**
+	 * Set the <code>DataStore</code> identifier.  This method is intended to
+	 * be used by a <code>DataStore</code> when the <code>Network</code>
+	 * instance is loaded, or by the <code>NetworkBuilder</code> implementation
+	 * to set the <code>DataStore</code> identifier, prior to storing a new
+	 * <code>Network</code> instance.
+	 *
+	 * @param  id The <code>DataStore</code> identifier, not null
+	 */
+
+	protected void setId (final Long id)
+	{
+		this.id = id;
+	}
+
+	/**
+	 * Get the name of the <code>Network</code>.
+	 *
+	 * @return A <code>String</code> containing the name of the
+	 *         <code>Network</code>
+	 */
+
+	@Override
+	public String getName()
+	{
+		return this.name;
+	}
+
+	/**
+	 * Set the name of the <code>Network</code>.  This method is intended to be
+	 * used by a <code>DataStore</code> when the <code>Network</code> instance
+	 * is loaded.
+	 *
+	 * @param name The name of the <code>Network</code>
+	 */
+
+	protected void setName (final String name)
+	{
+		assert name != null : "name is NULL";
+
+		this.name = name;
+	}
+
+	/**
+	 * Get a <code>String</code> representation of the <code>Network</code>
+	 * instance, including the identifying fields.
+	 *
+	 * @return A <code>String</code> representation of the <code>Network</code>
+	 *         instance
+	 */
+
+	@Override
+	public String toString()
+	{
+		ToStringBuilder builder = new ToStringBuilder (this);
+
+		builder.append ("name", this.name);
+
+		return builder.toString ();
+	}
+}
