cbd3abe038e08798265e97b1ad1c0427097f859d
James E. Stark <jstark@ieee.org>
2015-11-05 15:35:19 -0500

    Add package level documentation, and jsr305 annotations

---
+++ src/main/java/ca/uoguelph/socs/icc/edm/domain/metadata/package-info.java
@@ -0,0 +1,31 @@
+/* Copyright (C) 2015 James E. Stark
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Meta-information for the <code>Element</code> interface classes.  The classes
+ * in this package are used to provide an abstract meta-data description of the
+ * <code>Element</code> interface classes, such that the user of the meta-data
+ * can process an <code>Element</code> instance without knowing the details of
+ * its interface.
+ *
+ * @author  James.E.Stark
+ * @version 1.0
+ */
+
+@ParametersAreNonnullByDefault
+package ca.uoguelph.socs.icc.edm.domain.metadata;
+
+import javax.annotation.ParametersAreNonnullByDefault;
