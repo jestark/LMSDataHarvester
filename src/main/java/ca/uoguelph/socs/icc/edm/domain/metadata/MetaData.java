132499cd4eb762fed32be0b75cca7bbc50dd98ba
James E. Stark <jstark@ieee.org>
2015-11-05 14:15:15 -0500

    Add a references to the Element interface class
    As well as equals, hashCode and toString methods.

--- src/main/java/ca/uoguelph/socs/icc/edm/domain/metadata/MetaData.java
+++ src/main/java/ca/uoguelph/socs/icc/edm/domain/metadata/MetaData.java
@@ -21,6 +21,7 @@ import java.util.Set;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Objects;
 
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -29,7 +30,7 @@ import javax.annotation.CheckReturnValue;
 import javax.annotation.Nullable;
 
 import com.google.common.base.Preconditions;
-import com.google.common.base.Verify;
+import com.google.common.base.MoreObjects;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -53,6 +54,9 @@ public final class MetaData<T extends Element>
 	/** The Logger */
 	private final Logger log;
 
+	/** The <code>Element</code> interface class */
+	private final Class<T> element;
+
 	/** The parent <code>Element</code> class */
 	private final @Nullable MetaData<? super T> parent;
 
@@ -65,8 +69,8 @@ public final class MetaData<T extends Element>
 	/** The <code>Accessor</code> instances for the interface */
 	private final Map<Property<?>, Accessor<T, ?>> accessors;
 
-	/** The <code>Reference</code> instances for the interface */
-	private final Map<Property<?>, Reference<T, ?>> references;
+	/** The <code>MultiAccessor</code> instances for the interface */
+	private final Map<Property<?>, MultiAccessor<T, ?>> multiaccessors;
 
 	/** The <code>Relationship</code> instances for the interface */
 	private final Map<Property<?>, Relationship<T, ?>> relationships;
@@ -75,56 +79,132 @@ public final class MetaData<T extends Element>
 	 * Get the <code>MetaDataBuilder</code> for the specified
 	 * <code>Element</code> interface class.
 	 *
-	 * @param  <T>    The <code>Element</code> interface type
-	 * @param  parent The <code>MetaData</code> for the parent class
+	 * @param  <T>     The <code>Element</code> interface type
+	 * @param  element The <code>Element</code> interface class, not null
 	 *
-	 * @return        The <code>MetaDataBuilder</code> instance
+	 * @return         The <code>MetaDataBuilder</code> instance
 	 */
 
-	public static <T extends Element> MetaDataBuilder builder (final @Nullable MetaData<? super T> parent)
+	public static <T extends Element> MetaDataBuilder<T> builder (final Class<T> element)
 	{
-		return new MetaDataBuilder<T> (parent);
+		return new MetaDataBuilder<T> (element, null);
+	}
+
+	/**
+	 * Get the <code>MetaDataBuilder</code> for the specified
+	 * <code>Element</code> interface class.
+	 *
+	 * @param  <T>     The <code>Element</code> interface type
+	 * @param  element The <code>Element</code> interface class, not null
+	 * @param  parent  The <code>MetaData</code> for the parent class
+	 *
+	 * @return         The <code>MetaDataBuilder</code> instance
+	 */
+
+	public static <T extends Element> MetaDataBuilder<T> builder (final Class<T> element, final MetaData<? super T> parent)
+	{
+		return new MetaDataBuilder<T> (element, parent);
 	}
 
 	/**
 	 * Create the <code>MetaData</code>.
 	 *
-	 * @param  parent        The parent </code>Element</code> class, not null
-	 * @param  properties    The <code>Set</code> of <code>Property</code>
-	 *                       instances, not null
-	 * @param  selectors     The <code>Set</code> of <code>Selector</code>
-	 *                       instances, not null
-	 * @param  relationships The <code>Map</code> of <code>Relationship</code>
-	 *                       instances, not null
-	 * @param  accessors     The <code>Map</code> of <code>Accessor</code>
-	 *                       instances, not null
-	 * @param  references    The <code>Map</code> of <code>Reference</code>
-	 *                       instances, not null
+	 * @param  element        The <code>Element</code> interface class, not null
+	 * @param  parent         The parent </code>Element</code> class, not null
+	 * @param  properties     The <code>Set</code> of <code>Property</code>
+	 *                        instances, not null
+	 * @param  selectors      The <code>Set</code> of <code>Selector</code>
+	 *                        instances, not null
+	 * @param  accessors      The <code>Map</code> of <code>Accessor</code>
+	 *                        instances, not null
+	 * @param  multiaccessors The <code>Map</code> of <code>MultiAccessor</code>
+	 *                        instances, not null
 	 */
 
-	protected MetaData (final MetaData<? super T> parent,
+	protected MetaData (final Class<T> element,
+			final MetaData<? super T> parent,
 			final Set<Property<?>> properties,
 			final Set<Selector> selectors,
 			final Map<Property<?>, Accessor<T, ?>> accessors,
-			final Map<Property<?>, Reference<T, ?>> references,
-			final Map<Property<?>, Relationship<T, ?>> relationships)
+			final Map<Property<?>, MultiAccessor<T, ?>> multiaccessors)
 	{
+		assert element != null : "element is NULL";
 		assert properties != null : "properties is NULL";
 		assert selectors != null : "selectors is NULL";
 		assert accessors != null : "accessors is NULL";
-		assert references != null : "references is NULL";
-		assert relationships != null : "relationships is NULL";
+		assert multiaccessors != null : "references is NULL";
 
 		this.log = LoggerFactory.getLogger (this.getClass ());
 
+		this.element = element;
 		this.parent = parent;
 
 		this.selectors = new HashSet<> (selectors);
 		this.properties = new HashSet<> (properties);
 
 		this.accessors = new HashMap<> (accessors);
-		this.references = new HashMap<> (references);
-		this.relationships = new HashMap<> (relationships);
+		this.multiaccessors = new HashMap<> (multiaccessors);
+
+		this.relationships = new HashMap<> ();
+	}
+
+	/**
+	 * Compare two <code>MetaData</code> instances to determine if
+	 * they are equal.
+	 *
+	 * @param  obj The <code>MetaData</code> instance to compare to
+	 *             the one represented by the called instance
+	 *
+	 * @return     <code>true</code> if the two <code>MetaData</code>
+	 *             instances are equal, <code>false</code> otherwise
+	 */
+
+	@Override
+	public boolean equals (final Object obj)
+	{
+		return (obj == this) ? true : (obj instanceof MetaData)
+				&& Objects.equals (this.element, ((MetaData) obj).element);
+	}
+
+	/**
+	 * Compute a hashCode for the <code>MetaData</code> instance.
+	 *
+	 * @return An <code>Integer</code> containing the hash code
+	 */
+
+	@Override
+	public int hashCode ()
+	{
+		return Objects.hash (this.element);
+	}
+
+	/**
+	 * Get a <code>String</code> representation of the
+	 * <code>MetaData</code> instance, including the identifying fields.
+	 *
+	 * @return A <code>String</code> representation of the
+	 *         <code>MetaData</code> instance
+	 */
+
+	@Override
+	@CheckReturnValue
+	public String toString ()
+	{
+		return MoreObjects.toStringHelper (this)
+			.add ("element", this.element)
+			.toString ();
+	}
+
+	/**
+	 * Get the <code>Element</code> interface class represented by this
+	 * <code>MetaData</code> instance.
+	 *
+	 * @return The <code>Element</code> interface class
+	 */
+
+	public Class<T> getElementClass ()
+	{
+		return this.element;
 	}
 
 	/**
@@ -160,14 +240,17 @@ public final class MetaData<T extends Element>
 		Preconditions.checkArgument (this.hasProperty (property), "Property not associated with this element: %s", property.getName ());
 		Preconditions.checkArgument (! property.hasFlags (Property.Flags.MULTIVALUED), "Property is Multi-Valued: %s", property.getName ());
 
-		Accessor<? super T, ?> accessor = this.accessors.get (property);
+		@SuppressWarnings ("unchecked")
+		Accessor<? super T, V> accessor = (Accessor<? super T, V>) this.accessors.get (property);
 
 		if ((accessor == null) && (this.parent != null))
 		{
 			accessor = this.parent.getAccessor (property);
 		}
 
-		return (Accessor<? super T, V>) accessor;
+		assert accessor != null : String.format ("No Accessor found for Property: %s", property.getName ());
+
+		return accessor;
 	}
 
 	/**
@@ -184,16 +267,17 @@ public final class MetaData<T extends Element>
 		Preconditions.checkNotNull (property, "property");
 		Preconditions.checkArgument (this.hasProperty (property), "Property not associated with this element: %s", property.getName ());
 
-		Reference<? super T, ?> reference = this.references.get (property);
+		@SuppressWarnings ("unchecked")
+		Reference<? super T, V> reference = (Reference<? super T, V>) ((property.hasFlags (Property.Flags.MULTIVALUED)) ? this.multiaccessors.get (property) : this.accessors.get (property));
 
 		if ((reference == null) && (this.parent != null))
 		{
-			reference = this.parent.getReferences (property);
+			reference = this.parent.getReference (property);
 		}
 
 		assert reference != null : String.format ("No Reference found for Property: %s", property.getName ());
 
-		return (Reference<? super T, V>) reference;
+		return reference;
 	}
 
 	/**
@@ -210,7 +294,8 @@ public final class MetaData<T extends Element>
 		Preconditions.checkNotNull (property, "property");
 		Preconditions.checkArgument (property.hasFlags (Property.Flags.RELATIONSHIP), "Property is not a relationship: %s", property.getName ());
 
-		Relationship<? super T, ?> relationship = this.relationships.get (property);
+		@SuppressWarnings ("unchecked")
+		Relationship<? super T, V> relationship = (Relationship<? super T, V>) this.relationships.get (property);
 
 		if ((relationship == null) && (this.parent != null))
 		{
@@ -219,7 +304,7 @@ public final class MetaData<T extends Element>
 
 		assert relationship != null : String.format ("Relationship does not exist for Property: %s", property.getName ());
 
-		return (Relationship<? super T, V>) relationship;
+		return relationship;
 	}
 
 	/**
@@ -229,11 +314,11 @@ public final class MetaData<T extends Element>
 	 * @return A <code>Stream</code> of <code>Property</code> instances
 	 */
 
-	public Stream<Property<?>> propertyStream ()
+	public Stream<Property<?>> properties ()
 	{
 		Stream<Property<?>> result = this.properties.stream ();
 
-		return (this.parent != null) ? this.parent.propertyStream ().concat (result)
+		return (this.parent != null) ? Stream.concat (this.parent.properties (), result)
 			: result;
 	}
 
@@ -244,13 +329,13 @@ public final class MetaData<T extends Element>
 	 * @return A <code>Stream</code> of <code>Relationship</code> instances
 	 */
 
-	public Stream<Relationship<? super T, ?>> relationshipStream ()
+	public Stream<Relationship<? super T, ?>> relationships ()
 	{
 		Stream<Relationship<? super T, ?>> result = this.relationships.entrySet ()
 			.stream ()
 			.map (x -> x.getValue ());
 
-		return (this.parent != null) ? this.parent.relationshipStream ().concat (result)
+		return (this.parent != null) ? Stream.concat (this.parent.relationships (), result)
 			: result;
 	}
 
@@ -261,11 +346,11 @@ public final class MetaData<T extends Element>
 	 * @return A <code>Stream</code> of <code>Selector</code> instances
 	 */
 
-	public Stream<Selector> selectorStream ()
+	public Stream<Selector> selectors ()
 	{
-		Stream<Property<?>> result = this.selectors.stream ();
+		Stream<Selector> result = this.selectors.stream ();
 
-		return (this.parent != null) ? this.parent.selectorStream ().concat (result)
+		return (this.parent != null) ? Stream.concat (this.parent.selectors (), result)
 			: result;
 	}
 }
